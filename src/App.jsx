571d88fef41bcffa7b6045f61c85d2431751c492
Mamun Howlader <mamun.howlader@nexdecade.com>
2024-12-24 23:41:50 +0600

    initilize oroject and others configuration

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <div className="text-center">
+                <h1 className=" font-bold text-red-500">Hello, Tailwind CSS!</h1>
+            </div>
+        </>
+    );
+}
+
+export default App;
